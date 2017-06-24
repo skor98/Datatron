5467e38d457b04ac8c8a311cef125788f6169bd5
imasgo <Masha123>
2017-06-24 13:43:57 +0300

    Masha profile local setup I'm so proud

--- config.py
+++ config.py
@@ -27,9 +27,21 @@ class DimaSettings:
     HOST = 'localhost'
 
 
-ss, ds = ServerSettings, DimaSettings
-SETTINGS = ds
+class MashaSettings:
+    PATH_TO_KNOWLEDGEBASE = r'C:\Users\The Cat Trex\PycharmProjects\Datatron\kb\knowledge_base.db'
+    PATH_TO_USER_DB = r'C:\Users\The Cat Trex\PycharmProjects\Datatron\db\users.db'
+    PATH_TO_SOLR_POST_JAR_FILE = r'C:\Users\The Cat Trex\Desktop\solr\solr-6.3.0\solr-6.3.0\example\exampledocs\post.jar"'
+    PATH_TO_FFMPEG = r'C:\Users\The Cat Trex\PycharmProjects\Datatron\ffmpeg\ffmpeg.exe'
+    PATH_TO_MINFIN_ATTACHMENTS = r'C:\Users\The Cat Trex\PycharmProjects\Datatron\data\minfin'
+    TELEGRAM_API_TOKEN = '417763822:AAHltJOUDJyI0amublz1WHgctzL7zkQR_Vo'  # MaryDatatronTestBot
+    # PATH_TO_SOLR_BIN_FOLDER = r'C:\Users\User\Desktop\solr\solr-6.3.0\solr-6.3.0\bin'
+    SOLR_MAIN_CORE = 'kb_3c'
+    SOLR_MINFIN_CORE = 'minfin'
+    ADMIN_TELEGRAM_ID = (164241807,)
+    HOST = 'localhost'
 
-FFMPEG_PATH_MASHA = r'C:\Users\The Cat Trex\PycharmProjects\OpenFinData\ffmpeg\bin\ffmpeg.exe'
-CNTK_SERVER_PATH = r'C:/Users/imon/Documents/Git/Datatron 2.0/OpenFinData/cntk'
+
+ss, ds, ms = ServerSettings, DimaSettings, MashaSettings
+SETTINGS = ms
+
 YANDEX_API_KEY = 'e05f5a12-8e05-4161-ad05-cf435a4e7d5b'
