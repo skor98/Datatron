a2c8c407a57017d97b66ebf9a4b7a4383d7bba41
DmitryEliseev <dima-eliseev10@rambler.ru>
2017-06-24 14:45:44 +0300

    Some insignificant changes in config

--- config.py
+++ config.py
@@ -1,3 +1,5 @@
+YANDEX_API_KEY = 'e05f5a12-8e05-4161-ad05-cf435a4e7d5b'
+
 class ServerSettings:
     PATH_TO_KNOWLEDGEBASE = r'C:\Users\imon\Documents\Git\Datatron_2.0\Datatron\kb\knowledge_base.db'
     PATH_TO_USER_DB = r'C:\Users\imon\Documents\Git\Datatron_2.0\Datatron\db\users.db'
@@ -6,7 +8,6 @@ class ServerSettings:
     PATH_TO_MINFIN_ATTACHMENTS = r'C:\Users\imon\Documents\Git\Datatron_2.0\Datatron\data\minfin'
     TELEGRAM_API_TOKEN = '231161869:AAFpafehgQl9V-5f6-1KvwjPkzhbgdqDflU'  # OpenFinData
     TELEGRAM_API_TOKEN_FINAL = '242536845:AAELC6ay-SVnaw0nIZHCZpsqw_KN8yiaD1U'  # Datatron
-    # PATH_TO_SOLR_BIN_FOLDER = r'C:/local/solr-6.4.2/bin'
     SOLR_MAIN_CORE = 'kb_3c'
     SOLR_MINFIN_CORE = 'new_minfin'
     ADMIN_TELEGRAM_ID = (65305591, 164241807, 139653713)
@@ -20,7 +21,6 @@ class DimaSettings:
     PATH_TO_FFMPEG = r'C:\Users\User\PycharmProjects\Datatron\ffmpeg\ffmpeg.exe'
     PATH_TO_MINFIN_ATTACHMENTS = r'C:\Users\User\PycharmProjects\Datatron\data\minfin'
     TELEGRAM_API_TOKEN = '371109250:AAE_0U6v5MKNcNSCZBmzIXVFIM8FPNCPqPc'  # Dimatest_Bot
-    # PATH_TO_SOLR_BIN_FOLDER = r'C:\Users\User\Desktop\solr\solr-6.3.0\solr-6.3.0\bin'
     SOLR_MAIN_CORE = 'kb_3c'
     SOLR_MINFIN_CORE = 'minfin'
     ADMIN_TELEGRAM_ID = (65305591, )
@@ -34,7 +34,6 @@ class MashaSettings:
     PATH_TO_FFMPEG = r'C:\Users\The Cat Trex\PycharmProjects\Datatron\ffmpeg\ffmpeg.exe'
     PATH_TO_MINFIN_ATTACHMENTS = r'C:\Users\The Cat Trex\PycharmProjects\Datatron\data\minfin'
     TELEGRAM_API_TOKEN = '417763822:AAHltJOUDJyI0amublz1WHgctzL7zkQR_Vo'  # MaryDatatronTestBot
-    # PATH_TO_SOLR_BIN_FOLDER = r'C:\Users\User\Desktop\solr\solr-6.3.0\solr-6.3.0\bin'
     SOLR_MAIN_CORE = 'kb_3c'
     SOLR_MINFIN_CORE = 'minfin'
     ADMIN_TELEGRAM_ID = (164241807,)
@@ -43,5 +42,3 @@ class MashaSettings:
 
 ss, ds, ms = ServerSettings, DimaSettings, MashaSettings
 SETTINGS = ms
-
-YANDEX_API_KEY = 'e05f5a12-8e05-4161-ad05-cf435a4e7d5b'
