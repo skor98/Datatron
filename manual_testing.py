a96bc7e3c962ff409962f2f3713fe786cb0d39e8
Aleksey Lobanov <alekseylobanov1@gmail.com>
2017-07-03 15:44:44 +0300

    pylint fixes for manual_testing.py

--- manual_testing.py
+++ manual_testing.py
@@ -1,8 +1,13 @@
-import requests
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
 import json
 import uuid
 import datetime
 from os import getcwd, listdir
+
+import requests
+
 from data_retrieving import DataRetrieving
 
 
@@ -12,7 +17,8 @@ def post_request_to_server(request):
 
 def cube_testing(local=True):
     """Метод для тестирования работы системы по кубам.
-    Тесты находятся в папке tests и имеют следующую структуру названия cube_<local/server>_<имя куба>
+    Тесты находятся в папке tests и имеют следующую структуру названия
+    cube_<local/server>_<имя куба>
 
     :param local: если True, то тестируется локальная система, если False, то стоящая на сервере
     :return: None
@@ -29,12 +35,12 @@ def cube_testing(local=True):
             test_files_paths.append(r'{}\{}'.format(test_path, file))
 
     for tf in test_files_paths:
-        with open(r'{}'.format(tf), 'r', encoding='utf-8') as f:
+        with open(r'{}'.format(tf), 'r', encoding='utf-8') as file_in:
             doc_name_output_str = 'Файл: {}'.format(tf.split('\\')[-1])
             testing_results.append(doc_name_output_str)
             print(doc_name_output_str)
 
-            for idx, line in enumerate(f):
+            for idx, line in enumerate(file_in):
                 line = ' '.join(line.split())
 
                 if line.startswith('*'):
@@ -42,7 +48,10 @@ def cube_testing(local=True):
 
                 req, answer = line.split(':')
                 if local:
-                    system_answer = json.loads(DataRetrieving.get_data(req, uuid.uuid4(), formatted=False).toJSON())
+                    system_answer = json.loads(DataRetrieving.get_data(
+                        req, uuid.uuid4(),
+                        formatted=False
+                    ).toJSON())
                 else:
                     system_answer = json.loads(post_request_to_server(req).text)
 
@@ -55,12 +64,16 @@ def cube_testing(local=True):
                         true_answers += 1
                         print(ars)
                     except AssertionError:
-                        ars = '{}. - Запрос "{}" отрабатывает некорректно (должны получать: {}, получаем: {})'
+                        ars = (
+                            '{}. - Запрос "{}" отрабатывает некорректно' +
+                            '(должны получать: {}, получаем: {})'
+                        )
                         ars = ars.format(idx, req, int(answer), response)
                         testing_results.append(ars)
                         print(ars)
                 else:
-                    ars = '{}. - Запрос "{}" вызвал ошибку: {}'.format(idx, req, system_answer['message'])
+                    ars = '{}. - Запрос "{}" вызвал ошибку: {}'
+                    ars = ars.format(idx, req, system_answer['message'])
                     testing_results.append(ars)
                     print(ars)
 
@@ -72,8 +85,8 @@ def cube_testing(local=True):
         else:
             file_name = file_name.format('server', current_datetime, true_answers, false_answers)
 
-        with open(r'{}\{}'.format(test_path, file_name), 'w', encoding='utf-8') as file:
-            file.write('\n'.join(testing_results))
+        with open(r'{}\{}'.format(test_path, file_name), 'w', encoding='utf-8') as file_out:
+            file_out.write('\n'.join(testing_results))
 
     print('Лог прогона записан в файл {}'.format(file_name))
 
@@ -116,15 +129,17 @@ def minfin_testing(local=True):
                 if response:
                     try:
                         assert question_id == str(response)
-                        ars = '{q_id}  + Запрос "{req}" отрабатывает корректно'.format(q_id=question_id, req=req)
+                        ars = '{q_id}  + Запрос "{req}" отрабатывает корректно'
+                        ars = ars.format(q_id=question_id, req=req)
                         testing_results.append(ars)
                         true_answers += 1
-                        print(ars)
                     except AssertionError:
-                        ars = '{q_id} - Запрос "{req}" отрабатывает некорректно (должны получать:{q_id}, получаем:{fl})'
+                        ars = (
+                            '{q_id} - Запрос "{req}" отрабатывает некорректно ' +
+                            '(должны получать:{q_id}, получаем:{fl})'
+                        )
                         ars = ars.format(q_id=question_id, req=req, fl=response)
                         testing_results.append(ars)
-                        print(ars)
                 else:
                     # TODO: подправить MSG
                     ars = '{q_id}  - Запрос "{req}" вызвал ошибку: {msg}'.format(
@@ -133,7 +148,7 @@ def minfin_testing(local=True):
                         msg='Не определена'
                     )
                     testing_results.append(ars)
-                    print(ars)
+                print(ars)
 
     current_datetime = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
     false_answers = len(testing_results) - true_answers - len(test_files_paths)
