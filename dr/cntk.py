3b2962f7598333363756ad588f42db3a458b2c16
Aleksey Lobanov <alekseylobanov1@gmail.com>
2017-07-04 13:34:13 +0300

    pylint fixes for dr/cntk.py

--- dr/cntk.py
+++ dr/cntk.py
@@ -1,4 +1,12 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+"""
+Взаимодействие с CNTK
+"""
+
 import re
+
 import requests
 
 
@@ -19,12 +27,16 @@ class CNTK:
 
     @staticmethod
     def get_data(user_request):
+        """Получает данные от CNTK, возвращает CNTKResult"""
         filtered_user_request = CNTK._preprocessing_request(user_request)
         tags = CNTK._send_request_to_cntk(filtered_user_request)
         return CNTKResult(True, tags)
 
 
 class CNTKResult:
+    """
+    Структура, хранящая теги после взаимодействия с CNTK
+    """
     def __init__(self, status=False, tags=''):
         self.status = status
         self.tags = tags
