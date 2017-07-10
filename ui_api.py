9b090c9ffe047b2e88d378d9361e1ff168e24373
Aleksey Lobanov <i@likemath.ru>
2017-07-10 16:44:09 +0300

    Добавлено логгирование времени работы API

--- ui_api.py
+++ ui_api.py
@@ -16,6 +16,7 @@ from flask_restful import reqparse, abort, Api, Resource
 
 from messenger_manager import MessengerManager
 import logs_helper  # pylint: disable=unused-import
+from logs_helper import time_with_message
 from config import SETTINGS, API_PORT
 # pylint: disable=no-self-use
 # pylint: disable=missing-docstring
@@ -35,6 +36,7 @@ def get_minfin_data():
 get_minfin_data.data = None
 
 
+@time_with_message("_read_minfin_data", "debug", 10)
 def _read_minfin_data():
     """
     Читает xlsx данные, соединяет в один документ и возвращает словарь с соотв. полями.
@@ -87,6 +89,7 @@ def is_valid_api_key(api_key):
 
 class VoiceQuery(Resource):
     """Обрабатывает отправку файлов голосом"""
+    @time_with_message("VoiceQuery API Get", "info", 7)
     def post(self):
         args = parser.parse_args()
 
@@ -129,6 +132,7 @@ class VoiceQuery(Resource):
 
 class TextQuery(Resource):
     """Обрабатывает простой текстовой зарос"""
+    @time_with_message("TextQuery API Get", "info", 4)
     def get(self):
         args = parser.parse_args()
         logging.info(args)
