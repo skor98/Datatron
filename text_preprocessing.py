3b38fcf46398bd75c72efbd993bbd6edce73f1dc
Aleksey Lobanov <i@likemath.ru>
2017-07-13 11:42:45 +0300

    Возможно, исправляет ошибку с nltk stop_words

--- text_preprocessing.py
+++ text_preprocessing.py
@@ -23,6 +23,21 @@ class TextPreprocessing:
         self.norming_style = 'lem'
         self.language = 'russian'
 
+        # TODO: что делать с вопросительными словами?
+        # Базовый набор стоп-слов
+        self.stop_words = stopwords.words(self.language)
+
+        # Удаление из него отрицательной частицы
+        self.stop_words.remove('не')
+
+        # Если вопросительные слова и другие частицы не должны быть
+        # удалены из запроса, так как отражают его смысл
+        if not delete_question_words:
+            delete_stop_words_list = ['кто', 'что', 'это', 'где', 'для', 'зачем', 'какой']
+            self.stop_words = [sw for sw in self.stop_words if sw not in delete_stop_words_list]
+
+        self.stop_words += "также иной год да нет -".split()
+
     def normalization(
             self,
             text,
@@ -51,23 +66,8 @@ class TextPreprocessing:
         if delete_repeatings:
             tokens = list(set(tokens))
 
-        # TODO: что делать с вопросительными словами?
-        # Базовый набор стоп-слов
-        stop_words = stopwords.words(self.language)
-
-        # Удаление из него отрицательной частицы
-        stop_words.remove('не')
-
-        # Если вопросительные слова и другие частицы не должны быть
-        # удалены из запроса, так как отражают его смысл
-        if not delete_question_words:
-            delete_stop_words_list = ['кто', 'что', 'это', 'где', 'для', 'зачем', 'какой']
-            stop_words = [sw for sw in stop_words if sw not in delete_stop_words_list]
-
-        stop_words += "также иной год да нет -".split()
-
         # Убираем стоп-слова
-        tokens = [t for t in tokens if t not in stop_words]
+        tokens = [t for t in tokens if t not in self.stop_words]
 
         normalized_request = ' '.join(tokens)
 
