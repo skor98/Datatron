db6d6af67162e22bf2bc1901fdf3a62948bfc100
Larousse Nikolaev <lanolesa@ya.ru>
2017-07-19 15:17:27 +0300

    Расширен инструментарий для деревьев

--- core/trees.py
+++ core/trees.py
@@ -4,6 +4,7 @@
 """Работа с деревьями."""
 
 import itertools
+from copy import copy
 
 # pylint: disable=too-few-public-methods
 
@@ -23,8 +24,8 @@ class TreeNode(object):
         if self.label is None:
             repr_label = ''
         else:
-            repr_label = ' <{}>'.format(self.label)
-        res = '[TreeNode{} at ({}, {}): {}terminal, '.format(
+            repr_label = ' #{}'.format(self.label)
+        res = '<TreeNode{} at ({}, {}): {}terminal, '.format(
             repr_label,
             *self.pos,
             '' if self.terminal else 'not ',
@@ -34,7 +35,7 @@ class TreeNode(object):
                 len(self.edges_out),
                 '' if len(self.edges_out) == 1 else 's'
             )
-        return res + 'content is {}]'.format(self.content)
+        return res + 'content is {}>'.format(self.content)
 
 
 class TreeEdge(object):
@@ -86,9 +87,9 @@ class TreeModel(object):
         elif isinstance(obj, TreeNode):
             layer, point = obj.pos
         elif isinstance(obj, str):
-            layer, point = self.nodes.index[obj]
+            layer, point = self.nodes.index[obj.strip('#')]
         else:
-            raise TypeError
+            raise (TypeError, 'Input must be 2-tuple, TreeNode or str, not {}'.format(type(obj)))
 
         if layer < 0:
             layer += len(self)
@@ -171,6 +172,38 @@ class TreeModel(object):
         for edge in subtree.edges:
             self.edges.new(conv_pos(edge.origin.pos),
                            conv_pos(edge.target.pos), edge.weight)
+            
+    def pathfinder(self, start='root', eval_func=lambda e: e.weight, break_in_terminal=False, output_terminal=False, only_content=False):
+        """Поиск пути в дереве по заданным критериям.
+
+        :param start: Узел, из которого начинается поиск
+            (по умолчанию - корневой узел дерева).
+        :param lookup_func: Функция для расчёта приоритета рёбер. 
+            Должна принимать на вход объект TreeEdge и возвращать число,
+            прямо пропорциональное приоритету ребра. По умолчанию возвращает вес ребра.
+        :param break_in_terminal: Если True, то алгоритм остановится, как только придёт
+            в любой терминальный узел. По умолчанию - False.
+        :param output_terminal: Если True, то к выводу добавляется значение параметра
+            terminal финального узла пути. По умолчанию - False.
+        :param only_content: Если True, то в качестве пути возвращается список данных
+            из узлов; иначе возвращается список объектов рёбер. По умолчанию - False.
+        """
+        path = []
+        current = self[start]
+        
+        while len(self.edges.from_node(current)) > 0:
+            if break_in_terminal and current.terminal:
+                break
+            maxpath = max(self.edges.from_node(current, False), key=eval_func)
+            path.append(maxpath)
+            current = maxpath.target
+            
+        if only_content:
+            path = [e.target.content for e in path]
+        
+        if output_terminal:
+            return (current.terminal, path)
+        return path
 
     def __repr__(self):
         point = len(self.nodes)
@@ -188,7 +221,7 @@ class TreeNodesHandler(object):
 
     def __init__(self, tree):
         self._tree = tree
-        self.index = {'root': (0, 0)}
+        self.index = {}
 
     def new(self, layer=-1, label='', content=None, terminal=False, parents=(), weights=()):
         """Создаёт новый узел.
@@ -275,7 +308,7 @@ class TreeNodesHandler(object):
             return obj in self.index
         elif isinstance(obj, tuple):
             layer, point = self._tree.normpos(obj)
-            return (0 <= layer < len(self)) and (0 <= point < len(self[layer]))
+            return (0 <= layer < len(self._tree)) and (0 <= point < len(self._tree[layer]))
         return False
 
     def __len__(self):
@@ -302,7 +335,7 @@ class TreeEdgesHandler(object):
         else:
             TreeEdge(self._tree[or_pos], self._tree[tar_pos], weight)
 
-    def get(self, origin, target, default=None):
+    def get(self, origin, target, default=None, show_hidden=True):
         """Получает ребро по координатам его концов.
 
         :param origin: Координаты исходной точки ребра.
@@ -312,8 +345,8 @@ class TreeEdgesHandler(object):
 
         """
         or_pos, tar_pos = self._tree.normpos(origin), self._tree.normpos(target)
-        for edge in self._tree.structure[or_pos].edges_out:
-            if edge.target.pos == tar_pos:
+        for edge in self._tree[or_pos].edges_out:
+            if edge.target.pos == tar_pos and (show_hidden or edge.visible):
                 return edge
         return default
 
@@ -329,38 +362,63 @@ class TreeEdgesHandler(object):
             self._tree[or_pos].edges_out = [e for e in origin.edges_out if e.target.pos != tar_pos]
         if not getattr(self._tree[tar_pos], 'deleted', False):
             self._tree[tar_pos].edges_in = [e for e in target.edges_in if e.origin.pos != or_pos]
+            
+    
+    def hide(self, origin, target):
+        """Скрывает ребро (может учитываться при поиске пути).
+        
+        :param origin: Координаты исходной точки ребра.
+        :param target: Координаты конечной точки ребра.
 
-    def to_node(self, pos):
+        """
+        self.get(origin, target).visible = False
+        
+    def unhide(self, origin, target):
+        """Делает скрытое ребро видимым.
+        
+        :param origin: Координаты исходной точки ребра.
+        :param target: Координаты конечной точки ребра.
+
+        """
+        self.get(origin, target).visible = True
+        
+    def unhide_all(self):
+        """Делает все рёбра дерева видимыми."""
+        for e in self:
+            e.visible = True
+
+    def to_node(self, pos, show_hidden=True):
         """Список рёбер, входящих в узел дерева.
 
         :param pos: Координаты узла.
 
         """
-        return self._tree[pos].edges_in
-
-    def from_node(self, pos):
+        return [e for e in self._tree[pos].edges_in if (show_hidden or e.visible)]
+    
+    def from_node(self, pos, show_hidden=True):
         """Список рёбер, выходящих из узла дерева.
 
         :param pos: Координаты узла.
 
         """
-        return self._tree[pos].edges_out
+        return [e for e in self._tree[pos].edges_out if (show_hidden or e.visible)]
+        
 
-    def to_layer(self, layer):
+    def to_layer(self, layer, show_hidden=True):
         """Список рёбер, входящих в слой дерева.
 
         :param layer: Номер слоя.
 
         """
-        return sum((n.edges_in for n in self._tree[layer]), [])
-
-    def from_layer(self, layer):
+        return sum((self.to_node(n, show_hidden) for n in self._tree[layer]), [])
+        
+    def from_layer(self, layer, show_hidden=True):
         """Список рёбер, выходящих из слоя дерева.
 
         :param layer: Номер слоя.
 
         """
-        return sum((n.edges_out for n in self._tree[layer]), [])
+        return sum((self.from_node(n, show_hidden) for n in self._tree[layer]), [])
 
     def __getitem__(self, key):
         return self.get(*key)
@@ -377,3 +435,30 @@ class TreeEdgesHandler(object):
 
     def __len__(self):
         return len([i for i in self])
+    
+def piper(tree, return_path=False, **kwargs):
+    """Находит оптимальный путь в дереве, после чего собирает функцию из значений узлов пути.
+    
+    :param tree: Дерево, в котором ведётся поиск пути и определение функций.
+    :param return_path: Если True, возвращает не только получившуюся функцию, но и
+        получившийся путь.
+    :param **kwargs: Аргументы для функции tree.pathfinder()
+        
+    """
+    path = tree.pathfinder(**kwargs)
+    if kwargs.get('output_terminal', False):
+        pipe = path[1]
+    else:
+        pipe = path
+    if not kwargs.get('only_content', False):
+        pipe = [e.target.content for e in pipe]
+    
+    def pipe_func(val):
+        res = copy(val)
+        for func in pipe:
+            res = func(res)
+        return res
+    
+    if return_path:
+        return (pipe_func, path)
+    return pipe_func
