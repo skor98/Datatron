3bd50317b1cd2c58bb1557315f6702e22806bdf9
Larousse Nikolaev <lanolesa@ya.ru>
2017-07-27 16:43:29 +0300

    PEP8

--- core/ling_parser.py
+++ core/ling_parser.py
@@ -19,7 +19,7 @@ SYNONYMS = [
 ]
 
 class Word(object):
-    
+
     def __init__(self, parsed_word):
         if isinstance(parsed_word, str):
             parsed_word = Phrase.morph.parse(parsed_word)[0]
@@ -28,7 +28,7 @@ class Word(object):
         self.original = parsed_word
         self.normal = parsed_word.normal_form
         self.verbal = parsed_word.word
-        
+
     def inflect(self, grammemes):
         if isinstance(grammemes, (list, tuple)):
             return self.inflect(set(grammemes))
@@ -38,7 +38,7 @@ class Word(object):
         if not grammemes:
             return self.clone()
         return Word(self.original.inflect(grammemes))
-    
+
     def agrees(self, other):
         if isinstance(other, Phrase):
             if other.mainpos is None:
@@ -56,7 +56,7 @@ class Word(object):
                 return False
             match = True
         return match
-    
+
     def make_agree(self, other):
         if isinstance(other, Phrase):
             if other.mainpos is None:
@@ -71,21 +71,21 @@ class Word(object):
             if gr_pair[0] != gr_pair[1] and None not in gr_pair:
                 res = res.inflect(gr_pair[1])
         return res
-    
+
     def can_be_main(self):
         return {'NOUN', 'nomn'} in self
-    
+
     def accepts_accs(self):
         return (self.transitivity == 'tran'
                 or self.POS == 'PREP'
                 or self.case == 'accs')
-        
+
     def clone(self):
         return Word(self.original)
-    
+
     def __getattr__(self, attr):
         return getattr(self.original.tag, attr, None)
-    
+
     def __setattr__(self, attr, value):
         if attr in ('normal', 'verbal', 'original'):
             object.__setattr__(self, attr, value)
@@ -93,36 +93,36 @@ class Word(object):
             new = self.inflect(value)
             for key in new.__dict__:
                 object.__setattr__(self, key, getattr(new, key))
-        
+
     def __contains__(self, item):
         if isinstance(item, (list, tuple)):
             return set(item) in self
         if isinstance(item, str):
             return set(item.split(' ')) in self
         return item in self.original.tag
-    
+
     def __add__(self, other):
         return Phrase(self) + Phrase(other)
-    
+
     def __radd__(self, other):
         return Phrase(other) + Phrase(self)
-    
+
     def __eq__(self, other):
         return str(self.original) == str(getattr(other, 'original', None))
-    
+
     def __neq__(self, other):
         return not self == other
-    
+
     def __str__(self):
         return self.verbal
-    
+
     def __repr__(self):
         return self.original.__repr__()
 
 class Phrase(object):
-    
+
     morph = MorphAnalyzer()
-    
+
     def __init__(self, phrase, mainpos=None):
         if isinstance(phrase, (Phrase)):
             for key in phrase.__dict__:
@@ -142,20 +142,20 @@ class Phrase(object):
         else:
             main_finder = [n for n, w in enumerate(self.structure) if w.can_be_main()]
             self.mainpos = main_finder[0] if main_finder else None
-        
+
     def main(self):
         return None if self.mainpos is None else self[self.mainpos]
-            
+
     def inflect(self, grammemes):
         if self.mainpos is None:
             return self.clone()
         newmain = self.main().inflect(grammemes)
         return Phrase(
-                [w.make_agree(newmain) if w.agrees(self.main()) else w
-                 for w in self.structure],
-                self.mainpos
+            [w.make_agree(newmain) if w.agrees(self.main()) else w
+             for w in self.structure],
+            self.mainpos
         )
-    
+
     def find_in(self, context):
         if not isinstance(context, Phrase):
             return self.find_in(Phrase(context))
@@ -169,23 +169,23 @@ class Phrase(object):
             if word.normal in main_synonyms:
                 return num
         return None
-    
+
     def put_into(self, context, target_pos=None, preserve_number=False):
         if not isinstance(context, Phrase):
             return self.put_into(Phrase(context), target_pos, preserve_number)
         if target_pos is None:
             target_pos = self.find_in(context)
-            
+
         inflect_grammemes = [context[target_pos].case]
         if not preserve_number:
             inflect_grammemes.append(context[target_pos].number)
         new_phrase = self.inflect(inflect_grammemes)
-    
+
         new_context = []
         for num, word in enumerate(context):
             if num == target_pos \
                or (0 <= num-target_pos+self.mainpos < len(self)
-                   and self[num-target_pos+self.mainpos].normal == word.normal):
+                       and self[num-target_pos+self.mainpos].normal == word.normal):
                 new_context.append(None)
             elif word.agrees(context[target_pos]):
                 new_context.append(word.make_agree(new_phrase))
@@ -194,16 +194,16 @@ class Phrase(object):
 
         res = new_context[:target_pos] + new_phrase.structure + new_context[target_pos + 1:]
         return Phrase([w for w in res if w is not None])
-    
+
     def clone(self):
         return Phrase([w.clone() for w in self.structure], self.mainpos)
-    
+
     def __getitem__(self, key):
         if isinstance(key, int):
             return self.structure[key]
         res = [w for w in self.structure if w.verbal == key]
         return res[0] if res else None
-    
+
     def __contains__(self, item):
         if not isinstance(item, Phrase):
             return Phrase(item) in self
@@ -223,22 +223,22 @@ class Phrase(object):
         if not isinstance(other, Phrase):
             return self + Phrase(other)
         return Phrase(self.structure + other.structure)
-    
+
     def __radd__(self, other):
         return Phrase(other) + self
-    
+
     def __len__(self):
         return len(self.structure)
-    
+
     def __iter__(self):
         return iter(self.structure)
-    
+
     def __str__(self):
         return self.verbal
-    
+
     def __bool__(self):
         return bool(self.structure)
-            
+
     @staticmethod
     def parse(phrase):
         res = []
@@ -250,7 +250,7 @@ class Phrase(object):
             else:
                 res.append(Word(parsed[0]))
         return res
-    
+
     @staticmethod
     def unparse(phrase):
         return ' '.join(w.verbal for w in phrase)
