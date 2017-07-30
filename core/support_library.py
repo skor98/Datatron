bbf790c1461e8427c69c6de196e20e68809fda1e
DmitryEliseev <dima-eliseev10@rambler.ru>
2017-07-31 02:49:21 +0300

    Учет корректного куба из будущего классификатора

--- core/support_library.py
+++ core/support_library.py
@@ -464,6 +464,50 @@ def create_mdx_query(cube_data: CubeData, mdx_type='basic'):
         create_basic_mdx_query()
 
 
+def best_answer_depending_on_cube(cube_data_list: list, cube_name: str):
+    """
+    Выбор лучшего ответа по заданному кубу, если это возможно
+    """
+
+    # Для работы системы до появления классификатора
+    if not cube_name:
+        return
+
+    used_cube = cube_data_list[0].selected_cube['cube']
+
+    if used_cube != cube_name:
+        for cube_data in list(cube_data_list):
+            if cube_data.selected_cube['cube'] == cube_name:
+                cube_data_list.remove(cube_data)
+                cube_data_list.insert(0, cube_data)
+
+                logging.info(
+                    "Query_ID: {}\tMessage: {}".format(
+                        cube_data.request_id,
+                        (
+                            'Лучший ответ был сменен.'
+                            'Был куб {}, стал {}, '
+                            'лучший путь {}'.format(
+                                used_cube,
+                                cube_name,
+                                cube_data.tree_path
+                            )
+                        )
+                    )
+                )
+                return
+    else:
+        logging.info(
+            "Query_ID: {}\tMessage: {}".format(
+                cube_data_list[0].request_id,
+                (
+                    'Куб алгоритмически лучшего ответа'
+                    'совпадает с кубом из классификатора'
+                )
+            )
+        )
+
+
 def delete_repetitions(cube_data_list: list):
     """
     Удаление из результата после прогона дерева
