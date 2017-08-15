bf4f9b1eb9a8dc56ec95f73a95af21f29fb67aca
Larousse Nikolaev <lanolesa@ya.ru>
2017-08-15 12:52:13 +0300

    обновление парсера

--- core/parsers/time_parser.py
+++ core/parsers/time_parser.py
@@ -24,7 +24,8 @@ tp_time.add_many_subs({
     'г': 'год',
     'мес': 'месяц',
     'кв': 'квартал',
-    'полугодие': 'семестр'
+    'полугодие': 'семестр',
+    'полгода': 'семестр',
 })
 unit_lens = {
     'год': 12,
@@ -36,101 +37,82 @@ unit_lens = {
 anyunit = '|'.join(unit_lens.keys())
 unitcombo = '(?:(?:[0-9]+ )?(?:{}) ?)+'.format(anyunit)
 
-current_re = r'(?:текущий|нынешний|этот?|сегодняшний) (?P<unit>{})'.format(anyunit)
-last_re = r'(?:прошл(?:ый|ое)|предыдущий|прошедший|минувший) (?P<unit>{})'.format(anyunit)
-beforelast_re = r'(?P<pref>(?:поза[- ]?)+)прошлый (?P<unit>{})'.format(anyunit)
+points = r'(?:(?P<begin>начало|состояние|канун) |(?P<end>конец|итог|финал|окончание) )?'
+
+current_re = r'{}(?:текущий|нынешний|этот?|сегодняшний) (?P<unit>{})'.format(points, anyunit)
+last_re = r'{}(?P<pref>(?:поза[- ]?)*)(?:прошл(?:ый|ое)|предыдущий|прошедший|минувший) (?P<unit>{})'.format(points, anyunit)
+next_re = '{}(?P<pref>(?:после[- ]?)*)(следующий|будущ(?:ий|ее)|грядущий|наступающий) (?P<unit>{})'.format(points, anyunit)
 ago_re = r'(?P<interval>{}) назад'.format(unitcombo)
-next_re = '(следующий|будущий|грядущий|наступающий) (?P<unit>{})'.format(anyunit)
-afternext_re = '(?P<pref>(?:после[- ]?)+)следующий (?P<unit>{})'.format(anyunit)
-later_re = '(?:через )?(?:<interval>{})(?: спустя)?'.format(unitcombo)
+later_re = r'(?P<pr>через )?(?P<interval>{})(?(pr)| спустя)'.format(unitcombo)
 
 _dayformat = '(?P<day>[0-3]?[0-9])'
-_monthformat = '(?P<month>0?[0-9]|1[012]|{})'.format(anymonth)
+_monthformat = '(?P<month>0?[1-9]|1[012]|{})'.format(anymonth)
 _yearformat = r'(?P<year>(?:20)?[0-9][0-9])'
 dateformat_re = r'{}(?P<sep>[.\- /]){}(?:(?P=sep){})?'.format(
     _dayformat, _monthformat, _yearformat)
 
-static_re = r'(?P<num>[0-9]+) (?P<unit>{})(?: {})?'.format(anyunit, _yearformat)
+static_re = r'{}(?P<num>[0-9]+) (?P<unit>{})(?: {})?'.format(points, anyunit, _yearformat)
 
-@tp_time.re_handler(current_re, preserve_old=False)
+@tp_time.re_handler(current_re)
 def current_h(match):
-    return date_to_text(datetime.today(), match.group('unit'))
+    u_len = unit_lens.get(match.group('unit'), 1)
+    begin = bool(match.group('begin'))
+    newdate = process_units(datetime.today(), u_len, begin)
+    return date_to_text(newdate)
 
 
-@tp_time.re_handler(last_re, preserve_old=False)
+@tp_time.re_handler(last_re)
 def last_h(match):
-    u_len = unit_lens[match.group('unit')]
-    newdate = mod_date(datetime.today(), -u_len)
-    return date_to_text(newdate, match.group('unit'))
-
-
-@tp_time.re_handler(beforelast_re, preserve_old=False)
-def beforelast_h(match):
-    sum_len = unit_lens[match.group('unit')] * len(match.group('pref').split('з'))
+    u_len = unit_lens.get(match.group('unit'), 1)
+    sum_len = u_len * len(match.group('pref').split('з'))
+    begin = bool(match.group('begin'))
     newdate = mod_date(datetime.today(), -sum_len)
-    return date_to_text(newdate, match.group('unit'))
+    newdate = process_units(newdate, u_len, begin)
+    return date_to_text(newdate)
 
 
-@tp_time.re_handler(ago_re, preserve_old=True)
+@tp_time.re_handler(next_re)
+def next_h(match):
+    u_len = unit_lens.get(match.group('unit'), 1)
+    sum_len = u_len * len(match.group('pref').split('з'))
+    begin = bool(match.group('begin'))
+    newdate = mod_date(datetime.today(), sum_len)
+    newdate = process_units(newdate, u_len, begin)
+    return date_to_text(newdate) 
+
+
+@tp_time.re_handler(ago_re)
 def ago_h(match):
     words = match.group('interval').split(' ')
-    unit = max(unit_lens.keys(), key=unit_lens.get)
     sum_len = 0
     coeff = 1
     for word in words:
         if word.isnumeric():
             coeff = int(word)
         else:
-            sum_len += unit_lens[word] * coeff
+            sum_len += unit_lens.get(word, 0) * coeff
             coeff = 1
-            if unit_lens[word] < unit_lens[unit]:
-                unit = word
     newdate = mod_date(datetime.today(), -sum_len)
-    return date_to_text(newdate, unit)
+    return date_to_text(newdate)
 
 
-@tp_time.re_handler(next_re, preserve_old=False)
-def next_h(match):
-    u_len = unit_lens[match.group('unit')]
-    newdate = mod_date(datetime.today(), u_len)
-    return date_to_text(newdate, match.group('unit'))
-
-
-@tp_time.re_handler(afternext_re, preserve_old=False)
-def afternext_h(match):
-    sum_len = unit_lens[match.group('unit')] * len(match.group('pref').split('л'))
-    newdate = mod_date(datetime.today(), sum_len)
-    return date_to_text(newdate, match.group('unit'))
-
-
-@tp_time.re_handler(later_re, preserve_old=True)
+@tp_time.re_handler(later_re)
 def later_h(match):
     words = match.group('interval').split(' ')
-    unit = max(unit_lens.keys(), key=unit_lens.get)
     sum_len = 0
     coeff = 1
     for word in words:
         if word.isnumeric():
             coeff = int(word)
         else:
-            sum_len += unit_lens[word] * coeff
+            sum_len += unit_lens.get(word, 0) * coeff
             coeff = 1
-            if unit_lens[word] < unit_lens[unit]:
-                unit = word
     newdate = mod_date(datetime.today(), sum_len)
-    return date_to_text(newdate, unit)
+    return date_to_text(newdate)
 
 
-@tp_time.re_handler(static_re, preserve_old=False)
+@tp_time.re_handler(static_re)
 def static_h(match):
-    unit = match.group('unit')
-    u_len = unit_lens[unit]
-    if u_len == 12:
-        return match.group(0)
-    
-    num = int(match.group('num')) % (12//u_len)
-    if num == 0:
-        num = 12//u_len
     newyear = match.group('year')
     if not newyear:
         newyear = 1000
@@ -138,12 +120,15 @@ def static_h(match):
         newyear = int(newyear)
         if newyear < 100:
             newyear += 2000
-    newmonth = u_len * (num - 1) + 1
+    u_len = unit_lens.get(match.group('unit'), 1)
+    newmonth = u_len * int(match.group('num'))
+    begin = bool(match.group('begin'))
     newdate = datetime(year=newyear, month=newmonth, day=1)
-    return date_to_text(newdate, unit)
+    newdate = process_units(newdate, u_len, begin)
+    return date_to_text(newdate)
 
 
-@tp_time.re_handler(dateformat_re, preserve_old=False)
+@tp_time.re_handler(dateformat_re)
 def date_h(match):
     newyear = match.group('year')
     if not newyear:
@@ -154,37 +139,31 @@ def date_h(match):
             newyear += 2000
     newmonth = match.group('month')
     if newmonth.isnumeric():
-        newmonth = int(newmonth)
+        newmonth = max(min(int(newmonth), 12), 1)
     else:
         newmonth = norm_months.index(newmonth) + 1
     newday = int(match.group('day'))
     newdate = datetime(day=newday, month=newmonth, year=newyear)
-    if newday <= 10:
-        newdate = mod_date(newdate, -1)
-
-    unit = min(unit_lens.keys(), key=unit_lens.get)
-    return date_to_text(newdate, unit)
+    newdate = process_units(newdate, 1, newday <= 15)
+    return date_to_text(newdate)
 
 
-def date_to_text(date, unit):
-    u_len = unit_lens.get(unit, 1)
-    if u_len != 12:
-        u_end = date.month + u_len - ((date.month-1) % u_len) - 1
-        res_month = norm_months[u_end-1]
-        if date.year < 1200:
-            return res_month
-        return ' '.join([res_month, str(date.year)])
-    elif date.year < 1200:
-        return str(datetime.today().year)
-    return str(date.year)
+def process_units(date, u_len=1, begin=False):
+    if begin:
+        date = mod_date(date, -u_len)
+    newmonth = ((date.month - 1) // u_len + 1) * u_len
+    return datetime(year=date.year, month=newmonth, day=1)
+
+
+def date_to_text(date):
+    res_month = norm_months[date.month-1]
+    if date.year < 1200:
+        return res_month
+    return ' '.join([res_month, str(date.year)])
 
 
 def mod_date(date, delta):
-    abstime = date.month + (date.year * 12)
-    abstime += delta
-    newmonth = abstime % 12
-    newyear = abstime // 12
-    if newmonth == 0:
-        newyear -= 1
-        newmonth = 12
+    delta += date.month - 1
+    newmonth = (delta % 12) + 1
+    newyear = date.year + (delta // 12)
     return datetime(year=newyear, month=newmonth, day=1)
