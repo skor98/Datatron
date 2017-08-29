99a6c04c192a61c1fe9aaaac3249f1cada6c2b88
Aleksey Lobanov <i@likemath.ru>
2017-08-29 15:17:28 +0300

    Добавлена базовая поддержка pretty тестов дял классификатора

--- config.py
+++ config.py
@@ -66,6 +66,7 @@ MODEL_CONFIG_PATH = fixed_path("model.json")
 TEST_PATH_CUBE = fixed_path(path.join("tests", "cube"))
 TEST_PATH_MINFIN = fixed_path(path.join("tests", "minfin"))
 TEST_PATH_RESULTS = fixed_path(path.join("tests", "results"))
+FEEDBACK_TESTS_FOLDER = fixed_path(path.join("tests", "cubes_pretty"))
 
 DATA_PATH = fixed_path("data")
 
@@ -77,7 +78,6 @@ TECH_CUBE_DOCS_FILE = fixed_path(
 
 TECH_MINFIN_DOCS_FILE = 'tech_minfin_data_for_indexing.json'
 
-FEEDBACK_TESTS_FOLDER = fixed_path(path.join("tests", "cubes_pretty"))
 
 DATE_FORMAT = "%Y.%m.%d"
 TIME_FORMAT = "%H:%M:%S"
